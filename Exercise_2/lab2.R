6e4aac157804b253612c04bd40e8a48724a20948
Valerii Poliakov <valerii.poliakov@tietoevry.com>
2023-03-17 21:36:12 +0200

    directory renaming

---
+++ Exercise_2/lab2.R
@@ -0,0 +1,60 @@
+d <- read.csv(file="cars 1.csv", header=TRUE, sep=",")
+d
+
+#change the names
+e <- d$engine.displacement
+a <- d$acceleration
+
+#1. Identifying possible outliers
+
+#draw histograms
+hist(e, breaks=15)
+hist(e, breaks=50)
+hist(a, breaks=15)
+hist(a, breaks=50)
+
+#draw a scatter plots
+plot(e,a)
+plot(a,e)
+
+
+#2. Verifying possible outliers
+
+#Z-score standardization  
+zs.e <- (e-mean(e))/sd(e)
+zs.e
+#histogram after standardization
+hist(zs.e)
+#determine points whose values after standardization are less than -3 or greater than 3
+zs.e.outliers <- e[(zs.e < (-3)) | (zs.e > 3)]
+zs.e.outliers
+
+zs.a <- (a-mean(a))/sd(a)
+zs.a
+#histogram after standardization
+hist(zs.a)
+#determine points whose values after standardization are less than -3 or greater than 3
+zs.a.outliers <- a[(zs.a < (-3)) | (zs.a > 3)]
+zs.a.outliers
+
+
+#caclulate quartiles and outliers using the IQR method
+iqr.e.outliers <- e[(e < quantile(e,0.25) - 1.5*IQR(e)) | (e > quantile(e,0.75) + 1.5*IQR(e))]
+iqr.e.outliers
+
+iqr.a.outliers <- a[(a < quantile(a,0.25) - 1.5*IQR(a)) | (a > quantile(a,0.75) + 1.5*IQR(a))]
+iqr.a.outliers
+
+#4. Checking mean and median after removing outliers
+
+mean( e )
+median( e )
+mean( a )
+median( a )
+
+e_without_outliers = e[-which(e %in% c('740','850') )]
+a_without_outliers = a[-which(a %in% c('3','1.4','24.8','7','8') )]
+mean( e_without_outliers )
+median( e_without_outliers )
+mean( a_without_outliers )
+median( a_without_outliers )
