a7cdb46c0dc6e83e45308f8dffd86693e849c8b9
Dmytro H <odeskdima@gmail.com>
2023-05-11 18:11:38 +0300

    few small updates and pdf for nicer look

--- Exercise_5/Exercise_5.R
+++ Exercise_5/Exercise_5.R
@@ -21,10 +21,10 @@ for(i in seq(1, 20, by=2)){
   # k-fold cross validation with k=10
   for(k in seq(1:nbreaks)){
     test_indices <- which(folds==k,arr.ind=TRUE)
-    d.train <- d[-test_indices, 1:13]
-    d.train.class <- d[-test_indices, 14]
-    d.test <- d[test_indices,1:13]       #without class
-    d.test.class<- d[test_indices, 14]    #only class
+    d.train <- d[-test_indices, 1:13]     #train data without class
+    d.train.class <- d[-test_indices, 14] #train data with class only
+    d.test <- d[test_indices,1:13]        #test data without class
+    d.test.class<- d[test_indices, 14]    #test data with class only
     
     model<-C5.0(x=d.train, y=d.train.class, control=C5.0Control(CF=1.0, minCases = i))
     
@@ -52,7 +52,7 @@ for(i in seq(1, 20, by=2)){
 plot(ti, taer, type="l", ylim=c(0, 0.3), ylab="Test error rate", xlab="Train error rate", axes = FALSE, col="red")
 lines(ti, traer, col="green")
 xlabel <- seq(1, 20, by = 2)
-axis(1, at = xlabel)
+axis(1, at = xlabel) #set more informative x label
 axis(2)
 
 
@@ -64,16 +64,18 @@ n
 model<-rpart(class ~ ., data = d, method = "class", control=rpart.control(minsplit=2, cp=0.0))
 rpart.plot(model)
 
+# results of the CART algorithm for our new records
 pred <- predict(model, n, type="class")
 pred
 
 
 # C4.5
-d.train <- d[, 1:13]
-d.train.class <- d[, 14]
+d.train <- d[, 1:13]     #train data without class
+d.train.class <- d[, 14] #train data with class only
 
 model<-C5.0(x=d.train, y=d.train.class, control=C5.0Control(CF=1.0, minCases = 2))
 plot(model)
 
+# results of the C4.5 algorithm for our new records
 pred <- predict(model, n, type="class")
 pred
